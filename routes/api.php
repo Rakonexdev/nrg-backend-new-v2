b5733537bc7b66c7c95cf1e2da25a4bca3618a77
Rakonexdev <dev2@rakonex.com>
2026-07-27 15:36:16 +0530

    admin user delted is working

--- routes/api.php
+++ routes/api.php
@@ -172,6 +172,8 @@ Route::middleware(['auth:sanctum', 'check_login_time'])->group(function () {
         Route::get('/admin-users', [RoleController::class, 'adminUsers']);
         Route::post('/admin-users', [RoleController::class, 'createAdminUser']);
         Route::put('/admin-users/{id}', [RoleController::class, 'updateAdminUser']);
+        Route::match(['PUT', 'POST'], '/admin-users/{id}', [RoleController::class, 'updateAdminUser']);
+        Route::match(['DELETE', 'POST', 'GET'], '/admin-users/{id}/delete', [RoleController::class, 'deleteAdminUser']);
         Route::delete('/admin-users/{id}', [RoleController::class, 'deleteAdminUser']);
     });
 });
